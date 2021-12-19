1f79abb37f038ec7c1d0f2c29a5fb8b17ed693d2
Renato Pozzi <renato.pozzi.dev@gmail.com>
2021-12-20 00:38:59 +0100

    Create useTheme.js

---
+++ useTheme.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+
+function useTheme() {
+  const mq = "(prefers-color-scheme: dark)";
+
+  const [mode, setMode] = React.useState(() => {
+    if (window.localStorage.getItem("_useTheme")) {
+      return window.localStorage.getItem("_useTheme");
+    }
+
+    if (window.matchMedia && window.matchMedia(mq).matches) {                         
+      return "dark";
+    }
+
+    return "light";
+  });
+
+  React.useEffect(() => {
+    document.documentElement.className = mode;
+    window.localStorage.setItem("_useTheme", mode);
+  }, [mode]);
+
+  return [mode, setMode];
+}
+
+// And Enjoy
+const [theme, setTheme] = useTheme();
