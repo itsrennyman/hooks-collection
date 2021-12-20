93bb598299ad9b87e245894726ee79b67847b466
Renato Pozzi <renato.pozzi.dev@gmail.com>
2021-12-20 01:31:13 +0100

    Update useLocalStorage.js
    Added serialize & deserialize extensions, by default JSON.stringify & JSON.parse.

--- useLocalStorage.js
+++ useLocalStorage.js
@@ -1,19 +1,24 @@
 import * as React from 'react;
 
-function useLocalStorage(key, initialValue = "") {
+function useLocalStorage(
+  key,
+  initialValue = "",
+  options = { serialize: JSON.stringify, deserialize: JSON.parse }
+) {
+  const { serialize, deserialize } = options;
   const [value, setValue] = React.useState(() => {
     const existingValue = window.localStorage.getItem(key);
 
     if (existingValue) {
-      return JSON.parse(existingValue);
+      return deserialize(existingValue);
     }
 
     return initialValue;
   });
 
   React.useEffect(() => {
-    window.localStorage.setItem(key, JSON.stringify(value));
-  }, [key, value]);
+    window.localStorage.setItem(key, serialize(value));
+  }, [key, value, serialize]);
 
   return [value, setValue];
 }
