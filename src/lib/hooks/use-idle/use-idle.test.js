b25526616bdfd947097f289b3d89d4527b8bb4c4
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-02-21 23:05:26 +0100

    refactor: moved tests

--- src/lib/hooks/use-idle/use-idle.test.js
+++ src/lib/hooks/use-idle/use-idle.test.js
@@ -2,87 +2,85 @@ import { renderHook } from "@testing-library/react-hooks";
 import { act } from "react-dom/test-utils";
 import { useIdle } from "./use-idle";
 
-describe("use-idle", () => {
-  beforeEach(() => {
-    jest.useFakeTimers();
+beforeEach(() => {
+  jest.useFakeTimers();
+});
+
+it("should return idle state", () => {
+  const { result } = renderHook(() => useIdle(1000));
+  expect(result.current).toBe(true);
+});
+
+it("should return false on firing keypress event", () => {
+  const { result } = renderHook(() => useIdle(1000));
+
+  expect(result.current).toBe(true);
+
+  act(() => {
+    document.dispatchEvent(new KeyboardEvent("keypress"));
   });
 
-  it("should return idle state", () => {
-    const { result } = renderHook(() => useIdle(1000));
+  expect(result.current).toBe(false);
+
+  setTimeout(() => {
     expect(result.current).toBe(true);
+  }, 1001);
+});
+
+it("should return false on firing mousemove event", () => {
+  const { result } = renderHook(() => useIdle(1000));
+
+  expect(result.current).toBe(true);
+
+  act(() => {
+    document.dispatchEvent(new MouseEvent("mousemove"));
   });
 
-  it("should return false on firing keypress event", () => {
-    const { result } = renderHook(() => useIdle(1000));
+  expect(result.current).toBe(false);
 
+  setTimeout(() => {
     expect(result.current).toBe(true);
+  }, 1001);
+});
 
-    act(() => {
-      document.dispatchEvent(new KeyboardEvent("keypress"));
-    });
+it("should return false on firing touchmove event", () => {
+  const { result } = renderHook(() => useIdle(1000));
 
-    expect(result.current).toBe(false);
+  expect(result.current).toBe(true);
 
-    setTimeout(() => {
-      expect(result.current).toBe(true);
-    }, 1001);
+  act(() => {
+    document.dispatchEvent(new TouchEvent("touchmove"));
   });
 
-  it("should return false on firing mousemove event", () => {
-    const { result } = renderHook(() => useIdle(1000));
+  expect(result.current).toBe(false);
 
+  setTimeout(() => {
     expect(result.current).toBe(true);
+  }, 1001);
+});
 
-    act(() => {
-      document.dispatchEvent(new MouseEvent("mousemove"));
-    });
+it("should return false on multiple firing events", () => {
+  const { result } = renderHook(() => useIdle(1000));
 
-    expect(result.current).toBe(false);
+  expect(result.current).toBe(true);
 
-    setTimeout(() => {
-      expect(result.current).toBe(true);
-    }, 1001);
+  act(() => {
+    document.dispatchEvent(new KeyboardEvent("keypress"));
   });
 
-  it("should return false on firing touchmove event", () => {
-    const { result } = renderHook(() => useIdle(1000));
+  expect(result.current).toBe(false);
 
+  setTimeout(() => {
     expect(result.current).toBe(true);
+  }, 1001);
 
-    act(() => {
-      document.dispatchEvent(new TouchEvent("touchmove"));
-    });
-
-    expect(result.current).toBe(false);
-
-    setTimeout(() => {
-      expect(result.current).toBe(true);
-    }, 1001);
+  act(() => {
+    document.dispatchEvent(new MouseEvent("mousemove"));
   });
 
-  it("should return false on multiple firing events", () => {
-    const { result } = renderHook(() => useIdle(1000));
+  expect(result.current).toBe(false);
 
+  setTimeout(() => {
     expect(result.current).toBe(true);
-
-    act(() => {
-      document.dispatchEvent(new KeyboardEvent("keypress"));
-    });
-
-    expect(result.current).toBe(false);
-
-    setTimeout(() => {
-      expect(result.current).toBe(true);
-    }, 1001);
-
-    act(() => {
-      document.dispatchEvent(new MouseEvent("mousemove"));
-    });
-
-    expect(result.current).toBe(false);
-
-    setTimeout(() => {
-      expect(result.current).toBe(true);
-    }, 1001);
-  });
+  }, 1001);
 });
